a91eca94e47c760eb3ffe4d341e57de656a877cb
Bagus Artandyo W <artandyob@gmail.com>
2026-06-16 15:38:17 +0700

    adding configuration for front end

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
 <?php
-header("Location: Pages/login.php");
-exit;
-?>
+
+require 'config.php';
+
+
+if (!isset($_SESSION['api_token'])) {
+    header("Location: login.php");
+    exit;
+}
+
+
+$apiCall = callAPI('GET', '/pasien');
+
+
+$daftarPasien = [];
+if ($apiCall['status_code'] == 200 && isset($apiCall['response']['data'])) {
+    $daftarPasien = $apiCall['response']['data'];
+}
