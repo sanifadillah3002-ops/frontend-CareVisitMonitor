6a16a4e2d3e72fc194f400b63ae2ffa232b2d864
Bagus Artandyo W <artandyob@gmail.com>
2026-06-17 11:13:11 +0700

    chore: remove obsolete frontend configuration and entry files

--- Pages/cari-pasien.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Redirect old public search requests to the new unified jadwal page
-$q = $_GET['q'] ?? '';
-$redirectUrl = 'jadwal.php' . (!empty($q) ? '?q=' . urlencode($q) : '');
-header('Location: ' . $redirectUrl);
-exit;
-?>
